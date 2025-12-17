5a23e0853d9dcdf4686a2cef87e3fb27d5578338
wzwleo <leo20050312@gmail.com>
2025-12-18 05:20:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from groq import Groq
 from sidebar import render_sidebar
 
 def main():
@@ -23,6 +24,25 @@ def main():
         # 顯示基本統計資訊
         st.write("資料統計摘要：")
         st.write(df.describe())
-      
+    # 讀取 .env 裡的 KEY
+    load_dotenv()
+    API_KEY = os.getenv("GROQ_API_KEY")
+    
+    if API_KEY is None:
+        raise ValueError("請先設定 GROQ_API_KEY")
+    
+    # 設定 client
+    client = Groq(api_key=API_KEY)
+    
+    # 呼叫聊天 API
+    response = client.chat.completions.create(
+        model="openai/gpt-oss-20b",  # 使用 Groq 支援的模型
+        messages=[
+            {"role": "system", "content": "你是友善的助手"},
+            {"role": "user", "content": "用 Groq API 回答我吧！"}
+        ]
+    )
+    
+    print("AI 回應：", response.choices[0].message["content"])
 if __name__ == "__main__":
     main()
