1dfc05e54adab2072b7bfb6529d98c85ff3e1f2c
wzwleo <leo20050312@gmail.com>
2025-12-18 05:31:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
-import os
 import streamlit as st
-from groq import Groq
+import pandas as pd
 
-API_KEY = os.environ.get("GROQ_API_KEY")
+def main():
 
-if not API_KEY:
-    st.error("API Key 尚未設定！請在 Streamlit Secrets 裡新增 GROQ_API_KEY")
-else:
-    client = Groq(api_key=API_KEY)
+    st.set_page_config(layout="wide")
+    render_sidebar()
 
-    try:
-        response = client.chat.completions.create(
-            model="openai/gpt-oss-20b",
-            messages=[
-                {"role": "system", "content": "你是友善的助手"},
-                {"role": "user", "content": "請簡單介紹台灣夜市文化"}
-            ]
-        )
+    st.title("簡易模型建置")
+    st.write("___")
 
-        # 正確取得內容
-        content = getattr(response.choices[0].message, "content", None)
-        if content:
-            st.write(content)
-        else:
-            st.warning("回傳資料沒有 content 欄位")
+    uploaded_file = st.file_uploader("請上傳 CSV 檔案", type="csv")
 
-    except Exception as e:
-        st.error(f"呼叫 Groq API 失敗: {e}")
+    if uploaded_file is not None:
+        # 讀取 CSV
+        df = pd.read_csv(uploaded_file)
+
+        # 顯示前幾筆資料
+        st.write("資料前五筆：")
+        st.dataframe(df.head())
+
+        # 顯示基本統計資訊
+        st.write("資料統計摘要：")
+        st.write(df.describe())
+        
+if __name__ == "__main__":
+    main()
