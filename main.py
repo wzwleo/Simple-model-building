9228fa155765bb3f22eadd40e81d25d2cc53d3f5
wzwleo <leo20050312@gmail.com>
2025-12-18 05:06:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,23 +2,27 @@ import streamlit as st
 import pandas as pd
 from sidebar import render_sidebar
 
-render_sidebar()
-
-st.title("簡易模型建置")
-st.write("___")
-
-uploaded_file = st.file_uploader("請上傳 CSV 檔案", type="csv")
-
-if uploaded_file is not None:
-    # 讀取 CSV
-    df = pd.read_csv(uploaded_file)
+def main():
     
-    # 顯示前幾筆資料
-    st.write("資料前五筆：")
-    st.dataframe(df.head())
+    st.set_page_config(layout="wide")
+    render_sidebar()
     
-    # 顯示基本統計資訊
-    st.write("資料統計摘要：")
-    st.write(df.describe())
-  
-
+    st.title("簡易模型建置")
+    st.write("___")
+    
+    uploaded_file = st.file_uploader("請上傳 CSV 檔案", type="csv")
+    
+    if uploaded_file is not None:
+        # 讀取 CSV
+        df = pd.read_csv(uploaded_file)
+        
+        # 顯示前幾筆資料
+        st.write("資料前五筆：")
+        st.dataframe(df.head())
+        
+        # 顯示基本統計資訊
+        st.write("資料統計摘要：")
+        st.write(df.describe())
+      
+if __name__ == "__main__":
+    main()
